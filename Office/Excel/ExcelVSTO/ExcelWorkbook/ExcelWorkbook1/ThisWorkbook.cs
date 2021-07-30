1dc33b1cb4f3e4395831256417e6fcfe66630fb7
WXY <yisusheng666@gmail.com>
2021-07-30 12:40:17 +0800

    新增 Excel VSTO示例

---
+++ Office/Excel/ExcelVSTO/ExcelWorkbook/ExcelWorkbook1/ThisWorkbook.cs
@@ -0,0 +1,28 @@
+﻿namespace ExcelWorkbook1
+{
+    public partial class ThisWorkbook
+    {
+        private void ThisWorkbook_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisWorkbook_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO 设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisWorkbook_Startup);
+            this.Shutdown += new System.EventHandler(ThisWorkbook_Shutdown);
+        }
+
+        #endregion
+
+    }
+}
