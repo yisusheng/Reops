7ecb83e40b932b532d781009838085deac2b206d
WXY <yisusheng666@gmail.com>
2021-08-01 22:34:10 +0800

    更改

--- Python/pythonProject/AlienInvasion/alien_invasion.py
+++ Python/pythonProject/AlienInvasion/alien_invasion.py
@@ -1,7 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_funcions as gf
 
 
 def run_game():
@@ -13,14 +13,8 @@ def run_game():
     ship=Ship(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings,screen,ship)
 
 
 run_game()
