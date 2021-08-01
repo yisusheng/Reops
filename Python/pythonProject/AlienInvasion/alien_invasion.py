568ec8d213157f7649857ed12b40063b9f8f8ec5
WXY <yisusheng666@gmail.com>
2021-08-01 21:45:20 +0800

    更新

--- Python/pythonProject/AlienInvasion/alien_invasion.py
+++ Python/pythonProject/AlienInvasion/alien_invasion.py
@@ -2,9 +2,10 @@ import sys
 import pygame
 from settings import Settings
 
+
 def run_game():
     pygame.init()
-    ai_settings=Settings()
+    ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
@@ -14,7 +15,7 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill((ai_settings.bg_color))
+        screen.fill(ai_settings.bg_color)
 
         pygame.display.flip()
 
