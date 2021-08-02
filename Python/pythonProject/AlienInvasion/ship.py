4bc0a1e9851cc6914fd577db4918c666bad507bd
WXY <yisusheng666@gmail.com>
2021-08-02 15:11:32 +0800

    更改按键逻辑

--- Python/pythonProject/AlienInvasion/ship.py
+++ Python/pythonProject/AlienInvasion/ship.py
@@ -19,9 +19,9 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
         self.rect.centerx = self.center
