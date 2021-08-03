1f5d35afb0f8db997f49220c56179dea9eba9555
WXY <yisusheng666@gmail.com>
2021-08-03 16:39:05 +0800

    Update fib6.py

--- Python/pythonProject/Fibonacci/fib6.py
+++ Python/pythonProject/Fibonacci/fib6.py
@@ -1,13 +1,17 @@
-def fib6(n: int) -> int:
-    if n == 0:
-        return n
+from typing import Generator
+
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0
+    if n > 0:
+        yield 1
     last: int = 0
     next: int = 1
     for _ in range(1, n):
         last, next = next, last + next
-    return next
+        yield next
 
 
 if __name__ == "__main__":
-    print(fib6(5))
-    print(fib6(50))
+    for i in fib6(50):
+        print(i)
