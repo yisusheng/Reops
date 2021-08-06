970d89efc70c6b1eea9db36199c1d9f0dd50874c
WXY <yisusheng666@gmail.com>
2021-08-06 11:12:47 +0800

    更改 项目

---
+++ WPF/BaseOnFramwork/WPFFramwork/UsingListBox/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsingListBox
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVictor_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            DependencyObject level1 = VisualTreeHelper.GetParent(button);
+            DependencyObject level2 = VisualTreeHelper.GetParent(level1);
+            DependencyObject level3 = VisualTreeHelper.GetParent(level2);
+            MessageBox.Show(level3.GetType().ToString());
+        }
+    }
+}
