f7a46631892842fd63ba11894a8a9cc8600bdee5
WXY <yisusheng666@gmail.com>
2021-09-01 18:26:00 +0800

    新增项目

---
+++ WPF/BaseOnFramwork/WPFFramwork/TapTextBlock/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TapTextBlock
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtblk_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock txtblk = sender as TextBlock;
+            rand.NextBytes(rgb);
+            Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+    }
+}
