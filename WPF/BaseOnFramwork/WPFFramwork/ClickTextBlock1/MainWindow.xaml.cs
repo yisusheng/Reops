cde386b8a6cc32c4d7f55272cf08e0fced357206
WXY <yisusheng666@gmail.com>
2021-09-02 13:57:38 +0800

    新增

---
+++ WPF/BaseOnFramwork/WPFFramwork/ClickTextBlock1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClickTextBlock1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rand = new Random();
+        byte[] rgb = new byte[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is TextBlock)
+            {
+                TextBlock txtblk = e.OriginalSource as TextBlock;
+                rand.NextBytes(rgb);
+                Color clr = Color.FromArgb(255, rgb[0], rgb[1], rgb[2]);
+                txtblk.Foreground = new SolidColorBrush(clr);
+            }
+            base.OnMouseDown(e);
+        }
+    }
+}
