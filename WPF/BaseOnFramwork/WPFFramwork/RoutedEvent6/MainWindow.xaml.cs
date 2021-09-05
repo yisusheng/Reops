14dc5b0475990338974893f2efade05e1344353f
WXY <yisusheng666@gmail.com>
2021-09-05 15:40:09 +0800

    新建练习项目

---
+++ WPF/BaseOnFramwork/WPFFramwork/RoutedEvent6/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvent6
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AddHandler(UIElement.MouseDownEvent,
+                            new MouseButtonEventHandler(TextBlock_MouseDown),
+                            true);
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
