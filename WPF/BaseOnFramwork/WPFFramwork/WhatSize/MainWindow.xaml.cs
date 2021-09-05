0dd8148450c48c04409cdbf21cb69774f5106397
WXY <yisusheng666@gmail.com>
2021-09-05 16:16:21 +0800

    新增 WhatSize

---
+++ WPF/BaseOnFramwork/WPFFramwork/WhatSize/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WhatSize
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            widthText.Text = e.NewSize.Width.ToString();
+            heihtText.Text = e.NewSize.Height.ToString();
+        }
+    }
+}
