1ed3be7ee9c0899f4ac85ffdb7a5234445406914
WXY <yisusheng666@gmail.com>
2021-09-08 16:48:09 +0800

    Update Program.cs

--- DesignPatterns/SingletonPattern/SingletonPattern/Program.cs
+++ DesignPatterns/SingletonPattern/SingletonPattern/Program.cs
@@ -10,15 +10,25 @@ namespace SingletonPattern
     {
         static void Main(string[] args)
         {
+            TaskManager.getInstance();
+            TaskManager.getInstance();
         }
     }
 
     class TaskManager
     {
         private static TaskManager tm = null;
+        private static int number = 0;
         private TaskManager()
         {
             Console.WriteLine("初始化窗口。");
+            Console.ReadLine();
+        }
+
+        public void displayProcess()
+        {
+            Console.WriteLine("存在窗口{0}个",number);
+            Console.ReadLine();
         }
 
         public static TaskManager getInstance()
@@ -26,7 +36,9 @@ namespace SingletonPattern
             if(tm == null)
             {
                 tm = new TaskManager();
+                number += 1;
             }
+            tm.displayProcess();
             return tm;
         }
     }
