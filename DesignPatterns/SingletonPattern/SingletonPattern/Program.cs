8804f04fb87b7756ad2cfd39c4be16c9f83bfa2b
WXY <yisusheng666@gmail.com>
2021-09-08 16:26:57 +0800

    新增项目

---
+++ DesignPatterns/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class TaskManager
+    {
+        private static TaskManager tm = null;
+        private TaskManager()
+        {
+            Console.WriteLine("初始化窗口。");
+        }
+
+        public static TaskManager getInstance()
+        {
+            if(tm == null)
+            {
+                tm = new TaskManager();
+            }
+            return tm;
+        }
+    }
+}
