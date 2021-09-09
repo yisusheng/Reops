d1c888761e165de490dc103286e81c44550305c2
WXY <yisusheng666@gmail.com>
2021-09-09 14:50:54 +0800

    新增项目

---
+++ WPF/BaseOnFramwork/WPFFramwork/SimpleColorScroll/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleColorScroll
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void redSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+
+        //}
+
+        void OnSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            byte r = (byte)redSlider.Value;
+            byte g = (byte)GreenSlider.Value;
+            byte b = (byte)blueSlider.Value;
+
+            redValue.Text = r.ToString("X2");
+            GreenValue.Text = g.ToString("X2");
+            blueValue.Text = b.ToString("X2");
+
+            brushResult.Color = Color.FromArgb(255, r, g, b);
+        }
+    }
+}
