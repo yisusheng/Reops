a67fa3af6ea57bf1525ad42fb0605ce0a5f5e3f9
WXY <yisusheng666@gmail.com>
2021-09-11 21:29:58 +0800

    新增 项目

---
+++ TheDefinitiveReference/Event/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public delegate void PriceChangedHandler(decimal oldPrice, decimal newPrice);
+
+    public class Stock
+    {
+        string symbol;
+        decimal price;
+
+        public Stock(string symbol) { this.symbol = symbol; }
+
+        public event PriceChangedHandler PriceChanged;
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                if (PriceChanged != null)
+                    PriceChanged(oldPrice, price);
+            }
+        }
+    }
+}
