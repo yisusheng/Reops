84d558c17ef56c69fa9bd2a3f34d46b90685d87d
WXY <yisusheng666@gmail.com>
2022-01-05 15:54:08 +0800

    新增

---
+++ CSharp/VS/ProCSharp/SimpleClassExample/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        public string petName;
+        public int currSpeed;
+
+        public void PrintState()
+        {
+            Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+        }
+
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
