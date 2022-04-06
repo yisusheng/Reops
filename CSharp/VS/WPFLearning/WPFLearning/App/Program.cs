51d7f78ed1195df1a7027f14584bc90d8eae426b
WXY <yisusheng666@gmail.com>
2022-04-06 23:53:50 +0800

    新增

---
+++ CSharp/VS/WPFLearning/WPFLearning/App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace App
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application app=new Application();
+            Window1 window=new Window1();
+            window.Show();
+            app.Run();
+        }
+    }
+}
---
+++ CSharp/VS/WPFLearning/WPFLearning/App/Window1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace App
+{
+    class Window1 : Window
+    {
+        public Window1()
+        {
+            this.Title = "WPFWindow";
+
+            Button button = new Button();
+        }
+    }
+}
