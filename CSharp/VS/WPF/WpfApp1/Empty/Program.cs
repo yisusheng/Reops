553efb2de4c9981c26268dcd5f13b328699a8ff0
WXY <yisusheng666@gmail.com>
2022-04-06 23:22:07 +0800

    新增

---
+++ CSharp/VS/WPF/WpfApp1/Empty/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace Empty
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application app = new System.Windows.Application();
+            Window1 window = new Window1();
+        }
+    }
+}
