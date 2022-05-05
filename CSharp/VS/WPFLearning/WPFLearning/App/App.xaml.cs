a440497fde4ca4e37d0d68ffe231080cd521eccb
WXY <yisusheng666@gmail.com>
2022-05-05 20:03:09 +0800

    1

---
+++ CSharp/VS/WPFLearning/WPFLearning/App/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿// App.xaml.cs
+using System;
+using System.Windows;
+using App;
+namespace App1
+{
+    public partial class App : System.Windows.Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window1 window = new Window1();
+            window.Show();
+        }
+    }
+}
