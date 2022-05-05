99af1a98c04614a5177600124223841f8dc9367a
WXY <yisusheng666@gmail.com>
2022-05-05 20:02:30 +0800

    1

---
+++ CAD/demo/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+
+namespace ClassLibrary1
+{
+    public class LineDemo
+    {
+        [CommandMethod("LineDraw")]
+        public void LineDraw()
+        {
+            Line line = new Line();
+        }
+    }
+}
