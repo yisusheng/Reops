2a8d9ab0028aefbb75e36de406372b2150d2cba6
WXY <yisusheng666@gmail.com>
2022-06-25 11:33:06 +0800

    删除

--- flutter/testss/android/app/src/main/java/top/yisusheng/testss/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package top.yisusheng.testss;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- flutter/untitled/android/app/src/main/kotlin/top/yisusheng/untitled/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package top.yisusheng.untitled
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
